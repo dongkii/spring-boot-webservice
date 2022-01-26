ce77fd09a6ce3b775ff992c0b4e37ff42d6b0f46
dongkii <31609765+dongkii@users.noreply.github.com>
2022-01-27 00:05:56 +0900

    The Q2, Q3 for the 2nd week

--- src/test/java/com/dongkii/book/springboot/web/SolutionControllerTest.java
+++ src/test/java/com/dongkii/book/springboot/web/SolutionControllerTest.java
@@ -1,19 +1,19 @@
 package com.dongkii.book.springboot.web;
 
-import com.dongkii.book.springboot.web.solution.Report_results;
-import com.dongkii.book.springboot.web.solution.String_zip;
-import com.dongkii.book.springboot.web.solution.XInterval_NNumber;
+import com.dongkii.book.springboot.web.solution.*;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionControllerTest {
 
+    // 1st week Q1
     @Test
     public void 신고결과받기() {
         assertThat(Report_results.solution(new String[] {"muzi", "frodo", "apeach", "neo"}, new String[] {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"}, 2)).isEqualTo(new int[] {2, 1, 1, 0});
         assertThat(Report_results.solution(new String[] {"con", "ryan"}, new String[] {"ryan con", "ryan con", "ryan con", "ryan con"}, 3)).isEqualTo(new int[] {0, 0});
     }
 
+    // 1st week Q2
     @Test
     public void 문자열압축() {
         assertThat(String_zip.solution("aabbacc")).isEqualTo(7);
@@ -23,10 +23,28 @@ public class SolutionControllerTest {
         assertThat(String_zip.solution("xababcdcdababcdcd")).isEqualTo(17);
     }
 
+    // 2nd week Q1
     @Test
     public void x만큼_간격이_있는_n개의_숫자() {
         assertThat(XInterval_NNumber.solution(2, 5)).isEqualTo(new long[] {2, 4, 6, 8, 10});
         assertThat(XInterval_NNumber.solution(4, 3)).isEqualTo(new long[] {4, 8, 12});
         assertThat(XInterval_NNumber.solution(-4, 2)).isEqualTo(new long[] {-4, -8});
+    }
+
+    // 2nd week Q2
+    @Test
+    public void 더_맵게() {
+        assertThat(The_spicy.solution(new int[] {1, 2, 3, 9, 10, 12}, 7)).isEqualTo(2);
+        assertThat(The_spicy.solution(new int[] {5, 5, 6, 6, 9, 9}, 8)).isEqualTo(2);
+        assertThat(The_spicy.solution(new int[] {5, 5, 5, 5, 5, 5}, 7)).isEqualTo(3);
+        assertThat(The_spicy.solution(new int[] {1, 7, 9}, 7)).isEqualTo(1);
+    }
+
+    // 2nd week Q3
+    @Test
+    public void 오픈채팅방() {
+        assertThat(OpenChatting_room
+                .solution(new String[] {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"}))
+                .isEqualTo(new String[] {"Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."});
     }
 }
