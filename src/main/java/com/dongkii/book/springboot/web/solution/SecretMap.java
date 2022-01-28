86d30811189b9235877de2925ca1806200d554ca
dongkii <31609765+dongkii@users.noreply.github.com>
2022-01-28 16:04:56 +0900

    Update SecretMap.java

--- src/main/java/com/dongkii/book/springboot/web/solution/SecretMap.java
+++ src/main/java/com/dongkii/book/springboot/web/solution/SecretMap.java
@@ -4,25 +4,19 @@ import java.util.*;
 
 public class SecretMap {
     public static String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        String[] answer = new String[n];
 
         for(int i = 0; i < n; i++) {
-            int tmp1 = 0;
-            String str1 = "";
-            int tmp2 = 0;
-            String str2 = "";
+            int tmp1 = arr1[i];
+            int tmp2 = arr2[i];
+            answer[i] = "";
 
-            for(int j = 0; j < n; j++) {
-                System.out.println(arr1[i] % Math.pow(2, n-j-1));
-                str1 += String.valueOf(arr1[i] / Math.pow(2, n-j-1));
-                tmp1 = (int)(arr1[i] % Math.pow(2, n-j-1));
-
-                str2 += String.valueOf(arr2[i] / Math.pow(2, n-j-1));
-                tmp2 = (int)(arr2[i] % Math.pow(2, n-j-1));
+            for(int j = 0; j < n; j++) {        // arr배열의 각인덱스를 2^n-j-1 로 나누어 몫을 OR연산 처리, 나머지를 다시 계산
+                answer[i] += ((tmp1 / (int)Math.pow(2, n-j-1) | tmp2 / (int)Math.pow(2, n-j-1)) == 1? "#" : " ");   // 1일경우 #으로, 0일경우 공백으로
+                
+                tmp1 = (int)(tmp1 % (int)Math.pow(2, n-j-1));
+                tmp2 = (int)(tmp2 % (int)Math.pow(2, n-j-1));
             }
-
-            //System.out.println(str1);
-            //System.out.println(str2);
         }
 
         return answer;
