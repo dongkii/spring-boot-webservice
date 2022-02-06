e3961e25afc21d0ea377eac904b7037d7790b8bb
dongkii <31609765+dongkii@users.noreply.github.com>
2022-02-06 18:51:59 +0900

    jpa remark update

--- src/main/java/com/dongkii/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/dongkii/book/springboot/service/posts/PostsService.java
@@ -21,7 +21,8 @@ public class PostsService {
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        Posts posts = postsRepository.findById(id).orElseThrow(()
+                -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
@@ -29,7 +30,8 @@ public class PostsService {
     }
 
     public PostsResponseDto findById (Long id) {
-        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        Posts entity = postsRepository.findById(id).orElseThrow(()
+                -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
     }
--- src/test/java/com/dongkii/book/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/dongkii/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -51,7 +51,7 @@ public class PostsRepositoryTest {
     @Test
     public void BaseTimeEntity_등록() {
         //given
-        LocalDateTime now = LocalDateTime.of(2022, 2, 2, 0, 0, 0);
+        LocalDateTime now = LocalDateTime.of(2022, 2, 6, 0, 0, 0);
         postsRepository.save(Posts.builder()
                 .title("title")
                 .content("content")
