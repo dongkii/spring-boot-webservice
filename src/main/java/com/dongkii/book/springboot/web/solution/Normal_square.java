00ff61315251956c46426a4daa0459bf5e495c55
dongkii <31609765+dongkii@users.noreply.github.com>
2022-02-08 18:08:25 +0900

    normal square

--- src/main/java/com/dongkii/book/springboot/web/solution/Normal_square.java
+++ src/main/java/com/dongkii/book/springboot/web/solution/Normal_square.java
@@ -3,10 +3,12 @@ package com.dongkii.book.springboot.web.solution;
 public class Normal_square {
     public static long solution(int w, int h) {
         long answer = 1;
-    
+
+        int all = w * h;
+
         // 높이와 넓이가 같을 경우
         if( w == h ){
-            return w * w - w; 
+            return w * w - w;
         }
 
         // 항상 w가 크도록
@@ -16,9 +18,19 @@ public class Normal_square {
             w = tmp;
         }
 
+        // 최대공약수를 구함
         int cnt = gcd(w, h);
+        System.out.println(cnt);
 
-        answer = (w * h) - ((int)Math.pow(cnt, 2));
+        // 높이와 넓이를 최대공약수로 나누고 처리
+        w = w/cnt;
+        h = h/cnt;
+
+        // 대각선 길이를 구함
+        int line = (int)Math.ceil(Math.sqrt(Math.pow(w, 2) + Math.pow(h, 2)));
+
+        answer = all - (line * cnt);
+        System.out.println(answer);
 
         return answer;
     }
